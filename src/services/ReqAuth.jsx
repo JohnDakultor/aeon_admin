76c7a0973ad59b3957b72c4ade7b9e96f748cef8
CurlyProgrammer <140988688+CurlyProgrammer11@users.noreply.github.com>
2025-01-08 12:40:48 +0800

    updated design

--- src/services/ReqAuth.jsx
+++ src/services/ReqAuth.jsx
@@ -1,11 +1,11 @@
-import  { useAuth } from "./Authentication";
+import { useAuth } from "./Authentication";
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function RequireAuth ({children}){
-    const auth = useAuth();
-    if (!auth.user){
-        return <Navigate to='/'/>
-    }
-    return children;
+export default function RequireAuth({ children }) {
+  const auth = useAuth();
+  if (!auth.user) {
+    return <Navigate to="/" />;
+  }
+  return children;
 }
